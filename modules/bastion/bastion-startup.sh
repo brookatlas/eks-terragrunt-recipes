bbe32f255b5bfe8930b1c958ac0b9759b1f35b8e
brookatlas <brookatlas@gmail.com>
2024-05-18 22:36:27 +0300

    add terraform and terragrunt installation to bastion user-data

--- modules/bastion/bastion-startup.sh
+++ modules/bastion/bastion-startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install git
+sudo yum install git -y
+
 # remove old aws-cli
 sudo yum remove awscli
 sudo rm -rf /bin/aws /usr/bin/aws
@@ -15,3 +18,15 @@ sudo ln -s /usr/local/bin/aws /usr/bin/aws
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/bin/kubectl
 sudo chmod +x /usr/bin/kubectl
+
+# install terraform
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform-1.6.6-1.x86_64
+
+# install terragrunt
+sudo wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.55.19/terragrunt_linux_amd64
+sudo mv ./terragrunt_linux_amd64 /usr/local/bin/terragrunt
+sudo chmod u+x /usr/local/bin/terragrunt
+sudo ln -s /usr/local/bin/terragrunt /usr/bin/terragrunt
+
